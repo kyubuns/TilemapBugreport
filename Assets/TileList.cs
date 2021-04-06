aab5c9da4ad189cb95f6d683bb5910d49842e303
kyubuns <kyubuns@gmail.com>
2021-04-06 10:22:56 +0900

    再現

---
+++ Assets/TileList.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+[CreateAssetMenu(menuName = "Test/TileList")]
+public class TileList : ScriptableObject
+{
+    public TileBase red;
+    public TileBase green;
+    public TileBase blue;
+}
